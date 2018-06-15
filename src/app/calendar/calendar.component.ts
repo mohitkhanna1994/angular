de871ce86deaf773db1594ae492d6257d872afe4
mohit <mohitkhanna1994@gmail.com>
2018-06-15 20:25:06 +0530

    CSV upload fuctionality

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -19,4 +19,45 @@ getCalendars(){
   console.log(resp);
   this.calendars=resp.result});
 }
+// public changeListener(files: FileList){
+//   console.log(files);
+//   if(files && files.length > 0) {
+//      let file : File = files.item(0); 
+//        console.log(file.name);
+//        console.log(file.size);
+//        console.log(file.type);
+//        let reader: FileReader = new FileReader();
+//        reader.readAsText(file);
+//        reader.onload = (e) => {
+//           let csv: string = reader.result;
+//           console.log(csv);
+//        }
+//     }
+// }
+uploadDatasource(fileInput: any) {
+
+  let file = fileInput.target.files[0];
+  let fileName = file.name;
+  
+  
+  let payload = {
+    file,
+  }
+  
+  let formData: FormData = new FormData();
+  formData.append('file',file,file.name);
+  
+  
+  this.calendarService.uploadDatasource(formData)
+    .subscribe(
+      response => { 
+        console.log('UPLOADING success');
+  
+  
+      },
+      error => {
+        console.log('error',error)
+      });
+  
+  }
 }
--- src/app/calendar/calendar.service.ts
+++ src/app/calendar/calendar.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Event } from './Calendar';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Headers, Http, RequestOptions, Response, URLSearchParams } from '@angular/http';
+import { HttpResponse } from 'selenium-webdriver/http';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,15 @@ export class CalendarService {
 
   getCal():Observable<any>{
     return this.http.get<any>('http://localhost:7010/ws/calendar?userId=mohitkhanna1994@gmail.com');
+  }
+  uploadDatasource(payload): Observable<HttpResponse> {
+    let headers = new Headers();
+
+    headers.append('Accept', 'application/json, text/plain,');
+    let options = new RequestOptions({ headers: headers });
+
+
+    return this.http.post('http://192.:8080/vdg/admin/fetchVideos',payload, {observe: 'response'})
+      
   }
 }
